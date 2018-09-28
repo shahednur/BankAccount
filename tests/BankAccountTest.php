0a17037668a45832cecdf822e09c92ba7f67309f
shahednur <shahednurctg@gmail.com>
2018-09-29 05:00:34 +0600

    TDD balance function Test has been passed

--- tests/BankAccountTest.php
+++ tests/BankAccountTest.php
@@ -1,9 +1,15 @@
 <?php 
 require_once('../classes/BankAccount.php');
-
+require_once('Money.php');
 class BankAccountTest extends PHPUnit_Framework_TestCase
 {
+       public function testBalance()
+       {
+           $bank = new BankAccount(new Money(1200));
+           $bank->balance();
 
+           var_dump($bank);
+       }
 }
 
 ?>
