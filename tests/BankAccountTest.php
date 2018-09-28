f6aeb4444c30eefdee1c7e69a8a6d6309f0e9ae1
shahednur <shahednurctg@gmail.com>
2018-09-29 05:13:02 +0600

    deposit function has been passed the TDD Test

--- tests/BankAccountTest.php
+++ tests/BankAccountTest.php
@@ -1,6 +1,7 @@
 <?php 
 require_once('../classes/BankAccount.php');
 require_once('Money.php');
+
 class BankAccountTest extends PHPUnit_Framework_TestCase
 {
        public function testBalance()
@@ -9,6 +10,14 @@ class BankAccountTest extends PHPUnit_Framework_TestCase
            $bank->balance();
 
            var_dump($bank);
+       }
+
+       public function testDeposit()
+       {
+           $bank1  = new BankAccount(new Money(500));
+           $bank1->deposit(new Money(100));
+
+           var_dump($bank1);
        }
 }
 
