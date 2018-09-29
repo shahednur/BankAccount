7744f51158f45724c7fabe57b4f55c895afed243
shahednur <shahednurctg@gmail.com>
2018-09-29 17:39:28 +0600

    updating codes

--- tests/BankAccountTest.php
+++ tests/BankAccountTest.php
@@ -1,32 +1,7 @@
 <?php 
 require_once('../classes/BankAccount.php');
-// require_once('Money.php');
-class Money
-{
-    private $amount = 0;
+require_once('Money.php');
 
-    public function __construct($amount)
-    {
-        $this->amount = (float) $amount;
-    }
-
-    public function value()
-    {
-        return $this->amount;
-    }
-    public function increaseBalance($amount)
-    {
-        return $this->amount += $amount; // This helper function will increase the value of  balance.
-    }
-   public function decreaseBalance($amount)
-   {
-       return $this->amount -= $amount; // This helper function will decrease the value of balance.
-   }
-    public function __toString()
-    {
-        return (string) $this->value();
-    }
-}
 class BankAccountTest extends PHPUnit_Framework_TestCase
 {
        public function testBalance()
@@ -55,9 +30,9 @@ class BankAccountTest extends PHPUnit_Framework_TestCase
 
        public function testTransfer()
        { 
-           $bank3 =new BankAccount(new Money(500));
-           $bank4 =new BankAccount( new Money(700));
-           $bank3->transfer(new Money(200),$bank4);
+           $bank3 =new BankAccount(new Money(1000));
+           $bank4 =new BankAccount( new Money(1200));
+           $bank3->transfer(new Money(500),$bank4);
 
            var_dump($bank3);
        }
